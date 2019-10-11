7d41003bf989964638cc0286d8928ee1223a5cd3
joanpaon <joanpaon@gmail.com>
2019-10-11 19:51:24 +0200

    :art: Código optimizado

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 <pre>
 
 Project .....: LetraInicioAleatorio
-Summary .....: Java 8 | Netbeans 8.2 | Programas - Valores Aleatorios #06
+Summary .....: Programas - Valores Aleatorios #06
 Creation ....: 26/10/2016
-Modification : 
-Version .....: 
 Author ......: José A. Pacheco Ondoño - joanpaon@gmail.com
-Platform ....: Java 8
-IDE .........: NetBeans 8.2
+Platform ....: Java 11
+IDE .........: NetBeans 11
 Remote ......: https://github.com/joanpaon/LetraInicioAleatorio.git
 
 </pre>
--- src/org/japo/java/main/Main.java
+++ src/org/japo/java/main/Main.java
@@ -23,6 +23,9 @@ import java.util.Random;
  */
 public class Main {
 
+    // Instanciar Random
+    public static final Random RND = new Random();
+
     /**
      * @param args the command line arguments
      */
@@ -30,9 +33,6 @@ public class Main {
         // Constantes
         final char LETRA_INI = 'A';
         final char LETRA_FIN = 'Z';
-        
-        // Instanciar Random
-        Random rnd = new Random();
 
         // Variable
         char letra;
@@ -40,9 +40,9 @@ public class Main {
         // Generar dato
         // La suma de char puede exceder un char
         // El cast debe englobar todo
-        letra = (char) (rnd.nextInt(LETRA_FIN - LETRA_INI + 1) + LETRA_INI);
+        letra = (char) (RND.nextInt(LETRA_FIN - LETRA_INI + 1) + LETRA_INI);
 
         // Mensaje
-        System.out.printf("Letra de inicio de examen .: %c\n", letra);
+        System.out.printf("Letra de inicio de examen .: %c%n", letra);
     }
 }
